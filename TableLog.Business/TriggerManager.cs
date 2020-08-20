b98d07f435e440a6d5ea30e8696a4e897b543da4
George Nairooze <gnairooze@gmail.com>
2020-08-20 23:14:12 +0200

    refactor trigger manager class

--- TableLog.Business/TriggerManager.cs
+++ TableLog.Business/TriggerManager.cs
@@ -11,22 +11,16 @@ namespace TableLog.Business
             this._TableManager = tableManager;
         }
 
-        public string GenerateTriggerOnInsert(string originalConnectionString, string originalDbName, string originalSchema, string originalTableName, string targetDbName, string targetSchema)
+        private string generateTriggerStart(string originalDbName, string originalTableFullName, string triggerName, string triggerAction)
         {
             StringBuilder result = new StringBuilder();
-            string logTableName = $"{originalTableName}_Log";
-            string logTableFullName = $"[{ targetSchema}].[{ logTableName}]";
-            string triggerName = $"[{originalSchema}].[{originalTableName}_Log_On_Insert]";
-            string originalTableFullName = $"[{ originalSchema}].[{ originalTableName}]";
-
-            Models.Table table = _TableManager.ReadTableSchema(originalConnectionString, originalTableName);
 
             result.AppendLine($"use [{originalDbName}]");
             result.AppendLine($"go");
             result.AppendLine();
             result.AppendLine($"create trigger {triggerName}");
             result.AppendLine($"\ton {originalTableFullName}");
-            result.AppendLine($"\tafter insert");
+            result.AppendLine($"\tafter {triggerAction}");
             result.AppendLine($"as");
             result.AppendLine($"begin");
             result.AppendLine($"\tset nocount on;");
@@ -34,6 +28,15 @@ namespace TableLog.Business
             result.AppendLine($"\tdeclare @Correlation_ID uniqueidentifier");
             result.AppendLine($"\tset @Correlation_ID = newid()");
             result.AppendLine();
+
+            return result.ToString();
+        }
+        private string generateCommonInsert(Models.Table table, string targetDbName, string logTableFullName, int isOld, string triggerAction, string triggerTable)
+        {
+            StringBuilder result = new StringBuilder();
+            string originalTableName = table.Name;
+
+
             result.AppendLine($"\tinsert [{targetDbName}].{logTableFullName}");
             result.AppendLine($"\t(");
             result.AppendLine($"\t\t[Is_Old],");
@@ -43,24 +46,51 @@ namespace TableLog.Business
             {
                 Models.Column column = table.Columns[i];
 
-                result.AppendLine($"\t\t[{CalculateColumnName(originalTableName, column)}]{DetermineLastComma(i,table.Columns.Count)}");
+                result.AppendLine($"\t\t[{CalculateColumnName(originalTableName, column)}]{DetermineLastComma(i, table.Columns.Count)}");
             }
             result.AppendLine($"\t)");
             result.AppendLine($"\tselect");
-            result.AppendLine($"\t\t0,");
+            result.AppendLine($"\t\t{isOld},");
             result.AppendLine($"\t\t@Correlation_ID,");
-            result.AppendLine($"\t\t'insert',");
+            result.AppendLine($"\t\t'{triggerAction}',");
             for (int i = 0; i < table.Columns.Count; i++)
             {
                 Models.Column column = table.Columns[i];
 
                 result.AppendLine($"\t\t[{column.Name}]{DetermineLastComma(i, table.Columns.Count)}");
             }
-            result.AppendLine($"\tfrom inserted");
+            result.AppendLine($"\tfrom {triggerTable}");
+
+            return result.ToString();
+        }
+        private string generateTriggerEnd()
+        {
+            StringBuilder result = new StringBuilder();
+
             result.AppendLine($"end");
             result.AppendLine();
             result.AppendLine($"go");
             result.AppendLine();
+
+            return result.ToString();
+        }
+        public string GenerateTriggerOnInsert(string originalConnectionString, string originalDbName, string originalSchema, string originalTableName, string targetDbName, string targetSchema)
+        {
+            StringBuilder result = new StringBuilder();
+            string logTableName = $"{originalTableName}_Log";
+            string logTableFullName = $"[{ targetSchema}].[{ logTableName}]";
+            string triggerName = $"[{originalSchema}].[{originalTableName}_Log_On_Insert]";
+            string originalTableFullName = $"[{ originalSchema}].[{ originalTableName}]";
+            string triggerAction = "insert";
+            string triggerTable = "inserted";
+            int isOld = 0;
+
+            Models.Table table = _TableManager.ReadTableSchema(originalConnectionString, originalTableName);
+
+            result.AppendLine(generateTriggerStart(originalDbName, originalTableFullName, triggerName, triggerAction));
+            result.AppendLine(generateCommonInsert(table, targetDbName, logTableFullName, isOld, triggerAction, triggerTable));
+            result.AppendLine(generateTriggerEnd());
+            
 
             return result.ToString();
         }
@@ -72,49 +102,15 @@ namespace TableLog.Business
             string logTableFullName = $"[{ targetSchema}].[{ logTableName}]";
             string triggerName = $"[{originalSchema}].[{originalTableName}_Log_On_Delete]";
             string originalTableFullName = $"[{ originalSchema}].[{ originalTableName}]";
+            string triggerAction = "delete";
+            string triggerTable = "deleted";
+            int isOld = 1;
 
             Models.Table table = _TableManager.ReadTableSchema(originalConnectionString, originalTableName);
 
-            result.AppendLine($"use [{originalDbName}]");
-            result.AppendLine($"go");
-            result.AppendLine();
-            result.AppendLine($"create trigger {triggerName}");
-            result.AppendLine($"\ton {originalTableFullName}");
-            result.AppendLine($"\tafter delete");
-            result.AppendLine($"as");
-            result.AppendLine($"begin");
-            result.AppendLine($"\tset nocount on;");
-            result.AppendLine();
-            result.AppendLine($"\tdeclare @Correlation_ID uniqueidentifier");
-            result.AppendLine($"\tset @Correlation_ID = newid()");
-            result.AppendLine();
-            result.AppendLine($"\tinsert [{targetDbName}].{logTableFullName}");
-            result.AppendLine($"\t(");
-            result.AppendLine($"\t\t[Is_Old],");
-            result.AppendLine($"\t\t[Correlation_ID],");
-            result.AppendLine($"\t\t[Action],");
-            for (int i = 0; i < table.Columns.Count; i++)
-            {
-                Models.Column column = table.Columns[i];
-
-                result.AppendLine($"\t\t[{CalculateColumnName(originalTableName, column)}]{DetermineLastComma(i, table.Columns.Count)}");
-            }
-            result.AppendLine($"\t)");
-            result.AppendLine($"\tselect");
-            result.AppendLine($"\t\t1,");
-            result.AppendLine($"\t\t@Correlation_ID,");
-            result.AppendLine($"\t\t'delete',");
-            for (int i = 0; i < table.Columns.Count; i++)
-            {
-                Models.Column column = table.Columns[i];
-
-                result.AppendLine($"\t\t[{column.Name}]{DetermineLastComma(i, table.Columns.Count)}");
-            }
-            result.AppendLine($"\tfrom deleted");
-            result.AppendLine($"end");
-            result.AppendLine();
-            result.AppendLine($"go");
-            result.AppendLine();
+            result.AppendLine(generateTriggerStart(originalDbName, originalTableFullName, triggerName, triggerAction));
+            result.AppendLine(generateCommonInsert(table, targetDbName, logTableFullName, isOld, triggerAction, triggerTable));
+            result.AppendLine(generateTriggerEnd());
 
             return result.ToString();
         }
@@ -126,73 +122,18 @@ namespace TableLog.Business
             string logTableFullName = $"[{ targetSchema}].[{ logTableName}]";
             string triggerName = $"[{originalSchema}].[{originalTableName}_Log_On_Update]";
             string originalTableFullName = $"[{ originalSchema}].[{ originalTableName}]";
+            string triggerAction = "update";
+            string triggerTable = "deleted";
+            int isOld = 1;
 
             Models.Table table = _TableManager.ReadTableSchema(originalConnectionString, originalTableName);
 
-            result.AppendLine($"use [{originalDbName}]");
-            result.AppendLine($"go");
-            result.AppendLine();
-            result.AppendLine($"create trigger {triggerName}");
-            result.AppendLine($"\ton {originalTableFullName}");
-            result.AppendLine($"\tafter update");
-            result.AppendLine($"as");
-            result.AppendLine($"begin");
-            result.AppendLine($"\tset nocount on;");
-            result.AppendLine();
-            result.AppendLine($"\tdeclare @Correlation_ID uniqueidentifier");
-            result.AppendLine($"\tset @Correlation_ID = newid()");
-            result.AppendLine();
-            result.AppendLine($"\tinsert [{targetDbName}].{logTableFullName}");
-            result.AppendLine($"\t(");
-            result.AppendLine($"\t\t[Is_Old],");
-            result.AppendLine($"\t\t[Correlation_ID],");
-            result.AppendLine($"\t\t[Action],");
-            for (int i = 0; i < table.Columns.Count; i++)
-            {
-                Models.Column column = table.Columns[i];
-
-                result.AppendLine($"\t\t[{CalculateColumnName(originalTableName, column)}]{DetermineLastComma(i, table.Columns.Count)}");
-            }
-            result.AppendLine($"\t)");
-            result.AppendLine($"\tselect");
-            result.AppendLine($"\t\t1,");
-            result.AppendLine($"\t\t@Correlation_ID,");
-            result.AppendLine($"\t\t'update',");
-            for (int i = 0; i < table.Columns.Count; i++)
-            {
-                Models.Column column = table.Columns[i];
-
-                result.AppendLine($"\t\t[{column.Name}]{DetermineLastComma(i, table.Columns.Count)}");
-            }
-            result.AppendLine($"\tfrom deleted");
-            result.AppendLine();
-            result.AppendLine($"\tinsert [{targetDbName}].{logTableFullName}");
-            result.AppendLine($"\t(");
-            result.AppendLine($"\t\t[Is_Old],");
-            result.AppendLine($"\t\t[Correlation_ID],");
-            result.AppendLine($"\t\t[Action],");
-            for (int i = 0; i < table.Columns.Count; i++)
-            {
-                Models.Column column = table.Columns[i];
-
-                result.AppendLine($"\t\t[{CalculateColumnName(originalTableName, column)}]{DetermineLastComma(i, table.Columns.Count)}");
-            }
-            result.AppendLine($"\t)");
-            result.AppendLine($"\tselect");
-            result.AppendLine($"\t\t0,");
-            result.AppendLine($"\t\t@Correlation_ID,");
-            result.AppendLine($"\t\t'update',");
-            for (int i = 0; i < table.Columns.Count; i++)
-            {
-                Models.Column column = table.Columns[i];
-
-                result.AppendLine($"\t\t[{column.Name}]{DetermineLastComma(i, table.Columns.Count)}");
-            }
-            result.AppendLine($"\tfrom inserted");
-            result.AppendLine($"end");
-            result.AppendLine();
-            result.AppendLine($"go");
-            result.AppendLine();
+            result.AppendLine(generateTriggerStart(originalDbName, originalTableFullName, triggerName, triggerAction));
+            result.AppendLine(generateCommonInsert(table, targetDbName, logTableFullName, isOld, triggerAction, triggerTable));
+            triggerTable = "inserted";
+            isOld = 0;
+            result.AppendLine(generateCommonInsert(table, targetDbName, logTableFullName, isOld, triggerAction, triggerTable));
+            result.AppendLine(generateTriggerEnd());
 
             return result.ToString();
         }
